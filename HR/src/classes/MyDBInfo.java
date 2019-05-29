00e01730a688729e4b664f3ee6fa0a55c13cda9d
deme <dpipi17@freeuni.edu.ge>
2019-05-29 14:54:57 +0400

    "added DBInfo_Class"

---
+++ HR/src/classes/MyDBInfo.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class MyDBInfo {
+	public static final String MYSQL_USERNAME = "root";
+	public static final String MYSQL_PASSWORD = "";
+	public static final String MYSQL_DATABASE_SERVER = "";
+	public static final String MYSQL_DATABASE_NAME = "";
+}
