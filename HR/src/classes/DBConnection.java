278685ba55958472d877cd982ba5f70152278fd4
deme <dpipi17@freeuni.edu.ge>
2019-06-11 18:08:42 +0400

    "Company and Account Finished"

--- HR/src/classes/DBConnection.java
+++ HR/src/classes/DBConnection.java
@@ -1,8 +1,45 @@
 package classes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+	static String account = MyDBInfo.MYSQL_USERNAME;
+	static String password = MyDBInfo.MYSQL_PASSWORD;
+	static String server = MyDBInfo.MYSQL_DATABASE_SERVER;
+	static String database = MyDBInfo.MYSQL_DATABASE_NAME;
 	
-	public DBConnection() {
+	
+	
+	private static Connection con;
+	
+	public static Connection getCon() {
+		try {
+			
+			if(con == null) {
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://" + server, account, password);				
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		
+		return con;
 	}
+	
+	// closes connection
+	public static void closeCon() {
+		try {
+			if(con != null) {
+				con.close();				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
--- HR/src/classes/MyDBInfo.java
+++ HR/src/classes/MyDBInfo.java
@@ -2,7 +2,7 @@ package classes;
 
 public class MyDBInfo {
 	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "";
-	public static final String MYSQL_DATABASE_SERVER = "";
-	public static final String MYSQL_DATABASE_NAME = "";
+	public static final String MYSQL_PASSWORD = "Lebronjames232323";
+	public static final String MYSQL_DATABASE_SERVER = "localhost:3306";
+	public static final String MYSQL_DATABASE_NAME = "oop";
 }
